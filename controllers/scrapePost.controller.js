e3306efbb48d7493e0b82c01983ea932a47cf2f8
AMJordan886 <alvaromartinezjordan@yahoo.com>
2022-09-09 00:34:37 +0200

    Scrapper running

--- controllers/scrapePost.controller.js
+++ controllers/scrapePost.controller.js
@@ -1,5 +1,7 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs/promises");
 const { req, res } = require("express");
+
 
 
 
@@ -13,27 +15,48 @@ const scrapePost = async(req, res) => {
 
   await page.goto(url);
   
-   for (i = 1; i < 6; i++) {
+ /*    for (i = 1; i < 6; i++) {
      var element = await page.evaluate(() =>
     Array.from(document.querySelectorAll(".div")).map((a) => ({
-    
         result: a.querySelector('.any-div') && a.querySelector('.any-div').textContent.trim()
     }))
 );
-       
-       
-       
-       
-      /*  .waitForSelector(
-          // "#meanings > div.css-ixatld.e15rdun50 > ul > li:nth-child(" + i + ") > a"
-         "img"
-       ).then(() => console.log("First <span> from: " + url)); */
-
-
-
+      //   .waitForSelector(
+      //     // "#meanings > div.css-ixatld.e15rdun50 > ul > li:nth-child(" + i + ") > a"
+      //    "img"
+      //  ).then(() => console.log("First <span> from: " + url)); 
      var text = await page.evaluate((element) => element.textContent , element);
      console.log(text);
-   }
+} */ 
+
+
+
+  
+  const names = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll(".ficha-producto")).map(x => x.textContent)
+  })
+  await fs.writeFile("names.txt", names.join("\r\n"))
+
+/*   await page.click("#clickme")
+  const clickedData = await page.$eval("#data", el => el.textContent)
+  console.log(clickedData)
+
+  const photos = await page.$$eval("img", imgs => {
+    return imgs.map(x => x.src)
+  })
+
+  await page.type("#ourfield", "blue")
+  await Promise.all([page.click("#ourform button"), page.waitForNavigation()])
+  const info = await page.$eval("#message", el => el.textContent)
+
+  console.log(info)
+
+  for (const photo of photos) {
+    const imagepage = await page.goto(photo)
+    await fs.writeFile(photo.split("/").pop(), await imagepage.buffer())
+  } */
+
+
   await browser.close();
   
 
@@ -42,9 +65,8 @@ const scrapePost = async(req, res) => {
      status_title: "Scrapping",
      status_message: "caption",
      url: url,
-     text: text,
+     text: names,
    });
-  
   
   }
 
