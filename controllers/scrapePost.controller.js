b3ed9f64f8eebc2be5d7f1a5cd9c938ab24fbe98
AMJordan886 <alvaromartinezjordan@yahoo.com>
2022-09-10 23:24:11 +0200

    Scrapping with parameters

--- controllers/scrapePost.controller.js
+++ controllers/scrapePost.controller.js
@@ -5,17 +5,32 @@ const { req, res } = require("express");
 
 
 
+  /* 
+  url
+  parameters to extract or default to DEPAU, selector from warehouses
+  photos to client - View photos in client from url
+  Touch the photo to save this
+  
+  
+  */
+
+
+
+
+
+
+
 const scrapePost = async(req, res) => {
   const { url } = req.body;
-  console.log("URL FROM CONTROLLER")
-  console.log(url)
+  console.log("URL FROM CONTROLLER");
+  console.log(url);
 
-   const browser = await puppeteer.launch();
-   const page = await browser.newPage();
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
   await page.goto(url);
-  
- /*    for (i = 1; i < 6; i++) {
+
+  /*    for (i = 1; i < 6; i++) {
      var element = await page.evaluate(() =>
     Array.from(document.querySelectorAll(".div")).map((a) => ({
         result: a.querySelector('.any-div') && a.querySelector('.any-div').textContent.trim()
@@ -27,48 +42,68 @@ const scrapePost = async(req, res) => {
       //  ).then(() => console.log("First <span> from: " + url)); 
      var text = await page.evaluate((element) => element.textContent , element);
      console.log(text);
-} */ 
+} */
 
+  
+  
+  
+  
+
+  
+  
+  
+  
+  const names = await page.evaluate(() => {
+    return Array.from(
+      document.querySelectorAll(".ficha-producto div span")
+      // document.querySelectorAll(".precio")
+    ).map((x) => x.textContent);
+  });
+
+
+    const title = await page.evaluate(() => {
+    return Array.from(
+      document.querySelectorAll(".main-product h1")
+      // document.querySelectorAll(".precio")
+    ).map((x) => x.textContent);
+  });
 
 
   
-  const names = await page.evaluate(() => {
-    return Array.from(document.querySelectorAll(".ficha-producto")).map(x => x.textContent)
-  })
-  await fs.writeFile("names.txt", names.join("\r\n"))
+  await fs.writeFile("names.txt", names.join("\r\n"));
 
-/*   await page.click("#clickme")
-  const clickedData = await page.$eval("#data", el => el.textContent)
-  console.log(clickedData)
+  // await page.click("#clickme");
+  // const clickedData = await page.$eval("#data", (el) => el.textContent);
+  // console.log(clickedData);
 
-  const photos = await page.$$eval("img", imgs => {
-    return imgs.map(x => x.src)
-  })
+  const photos = await page.$$eval("img", (imgs) => {
+    return imgs.map((x) => x.src);
+  });
 
-  await page.type("#ourfield", "blue")
-  await Promise.all([page.click("#ourform button"), page.waitForNavigation()])
-  const info = await page.$eval("#message", el => el.textContent)
 
-  console.log(info)
+  
+  // await page.type("#ourfield", "blue")
+  // await Promise.all([page.click("#ourform button"), page.waitForNavigation()])
+  // const info = await page.$eval("#message", el => el.textContent)
+  // console.log(info)
 
   for (const photo of photos) {
     const imagepage = await page.goto(photo)
     await fs.writeFile(photo.split("/").pop(), await imagepage.buffer())
-  } */
-
+  } 
 
   await browser.close();
-  
 
-   res.status(200).json({
-     msg: "File created succesfully",
-     status_title: "Scrapping",
-     status_message: "caption",
-     url: url,
-     text: names,
-   });
-  
-  }
+  res.status(200).json({
+    msg: "File created succesfully",
+    status_title: "Scrapping",
+    status_message: "caption",
+    url: url,
+    text: names,
+    title: title.toString().toUpperCase(),
+    photos: photos
+  });
+}
 
 
 
